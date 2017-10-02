9e236eed07abe7c1a2692d423b4931ab7ae9df85
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-01 21:19:15 -1000

    simpler logger import

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,6 +1,6 @@
 import * as pgpModule from 'pg-promise';
 import {db as config} from '../config';
-import * as pino from 'pino';
+import { BaseLogger } from 'pino';
 import * as debugModule from 'debug';
 
 const debug = debugModule('services:db');
@@ -9,7 +9,7 @@ const pgp = pgpModule({});
 export const db = pgp(config.url);
 export const helpers = pgp.helpers;
 
-export async function query(logger : pino.BaseLogger, queryName: string, query : string, args? : any) : Promise<any[]> {
+export async function query(logger : BaseLogger, queryName: string, query : string, args? : any) : Promise<any[]> {
   debug("SQL query", { query, args });
   let start = Date.now();
   let result = await db.query(query, args);
