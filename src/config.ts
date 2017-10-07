edc88786f986ae63990cab9a12db192065dbbaee
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-07 01:48:06 -0700

    URL config for brokerage server

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,15 @@
 import * as _ from 'lodash';
 
 export const db = {
-  url: process.env.DB_URL || 'sqlite3://stakes.db',
+  url: process.env.DB_URL || 'postgres://localhost/stakes',
 };
 
 export const env = _.toLower(process.env.ENV || process.env.NODE_ENV || 'dev');
 export const dev = env === 'dev' || env === 'development';
 
+export const brokerage_server = {
+  url: process.env.BROKERAGE_SERVER_URL || 'http://localhost:6543',
+};
 
 const bindList = (process.env.BIND_HOST || '127.0.0.1:7834').split(':');
 let host;
