f6f8ecb3952d394f3f595634598633a4d8cc173f
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-07 01:47:49 -0700

    Query helper can be called with a tx object

--- src/services/db.ts
+++ src/services/db.ts
@@ -6,13 +6,17 @@ import * as debugModule from 'debug';
 const debug = debugModule('services:db');
 
 const pgp = pgpModule({});
-export const db = pgp(config.url);
+export const pg = pgp(config.url);
 export const helpers = pgp.helpers;
 
-export async function query(logger : BaseLogger, queryName: string, query : string, args? : any) : Promise<any[]> {
-  debug("SQL query", { query, args });
+export async function queryTx(logger : BaseLogger, queryName: string, tx : pgpModule.IDatabase<any> | pgpModule.ITask<any>, query : string, args? : any) : Promise<any[]> {
+  debug("SQL query", { queryName, query, args });
   let start = Date.now();
-  let result = await db.query(query, args);
+  let result = await tx.query(query, args);
   logger.info({args}, "SQL query %s took %d ms", queryName, Date.now() - start);
   return result;
+}
+
+export function query(logger: BaseLogger, queryName: string, query: string, args?: any) {
+  return queryTx(logger, queryName, pg, query, args);
 }
