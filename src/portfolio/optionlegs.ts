711455811ea0c55772694cb5c7dca5a60364e526
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-07 01:48:28 -0700

    Fill in some option leg model

--- src/portfolio/optionlegs.ts
+++ src/portfolio/optionlegs.ts
@@ -1,1 +1,51 @@
-export const jsonObjectSyntax = `'id', ol.id, 'price', ol.price, 'size', ol.size, 'call', ol.call, 'expiration', ol.expiration, 'strike', ol.strike`;
+import * as models from '../common/models';
+import * as _ from 'lodash';
+import * as optionlegs from './optionlegs';
+import * as db from '../services/db';
+import { Request } from '../types';
+
+const Column = models.Column;
+
+export class OptionLeg {
+  @Column({ readonly: true })
+  id: string;
+  @Column({ readonly: true })
+  user_id: string;
+  @Column({ required: true })
+  symbol: string;
+  @Column({ required: true })
+  price: number;
+  @Column({ required: true })
+  size: number;
+  @Column({ required: true })
+  call: boolean;
+  @Column({ required: true })
+  expiration: Date;
+  @Column({ required: true })
+  strike: number;
+  @Column({ required: true })
+  commissions: number;
+  @Column({ required: true })
+  opening_trade: string;
+  @Column()
+  closing_trade: string;
+  @Column()
+  expired: boolean;
+}
+
+const { accessors: preMadeAccessors, schema } = models.makeAllData(OptionLeg, 'optionlegs');
+export { schema };
+
+export const jsonObjectSyntax = _.map(
+  models.schemaFieldList(schema.output, 'user_id', 'symbol'),
+  (v, k) => `'${k}', ol.${k}`
+).join(', ');
+
+export const accessors = {
+  ...preMadeAccessors,
+  split: (req : Request, id : string, splitOff: number) => {
+    // Split an option leg into two legs, identical except for the size. This is done to facilitate
+    // partial position closing.
+    throw new Error("Not implemented");
+  }
+};
