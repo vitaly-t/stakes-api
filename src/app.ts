c7b3dd1f530882948a9b3089a7469eef2429a27d
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-13 17:45:16 -1000

    use fastify-apollo fork

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import 'reflect-metadata';
 import * as fastifyMod from 'fastify';
 import * as config from './config';
 import * as fastifyApollo from 'fastify-apollo';
+import { Request, Reply } from './types';
 import schema from './models/schema';
 
 const fastify = fastifyMod({
@@ -12,13 +12,17 @@ const fastify = fastifyMod({
 });
 
 fastify.register(fastifyApollo, {
-  graphql: () => ({
-    schema,
-    context: {
-      user: { id: 1 } // hardcoded for now.
-    }
-  }),
-  graphiql: true
+  graphql: (req : Request, rep: Reply) => {
+    req.user = { id: 'e1bc5ec0-b08f-11e7-b258-0bc36fa3d63a' }; // hardcoded for now.
+    return {
+      schema,
+      context: {
+        req,
+        rep,
+      },
+    };
+  },
+  graphiql: true,
 } as any);
 
 fastify.listen(config.bind.port, config.bind.host, function(err) {
