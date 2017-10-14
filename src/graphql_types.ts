eb0dee2a23cd7b47b9dd452cd8a8f44d4ffd892f
Daniel Imfeld <daniel@danielimfeld.com>
2017-10-13 20:00:31 -1000

    uniqueKey can be an array

--- src/graphql_types.ts
+++ src/graphql_types.ts
@@ -3,7 +3,7 @@ import * as gq from 'graphql';
 declare module 'graphql/type/definition' {
   export interface GraphQLObjectTypeConfig<TSource, TContext> {
     sqlTable?: string;
-    uniqueKey?: string;
+    uniqueKey?: string | string[];
   }
 }
 
